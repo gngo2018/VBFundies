3900ee27753af83db0af03773d53bcaa360ce01b
George Go <gngo2018@gmail.com>
2019-06-23 16:28:47 -0400

    Initial Commit

---
+++ 00_HelloWorld/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        'VB more forgiving than C#, don't need ; use ' for comments
+        'Can't use cw tab tab shortcut for Console.Writeline
+        Console.WriteLine("Hello World")
+        Console.ReadLine()
+    End Sub
+
+End Module
